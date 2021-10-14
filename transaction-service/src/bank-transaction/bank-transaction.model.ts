02c752be6ca42618db26c106bb68816fa9b799fb
anhkhuong <anhkhuong975@gmail.com>
2021-10-14 17:56:12 +0700

    [api-service , transaction-service] insert db

--- transaction-service/src/bank-transaction/bank-transaction.model.ts
+++ transaction-service/src/bank-transaction/bank-transaction.model.ts
@@ -1,6 +1,8 @@
+import {TransactionTypeEnum} from "../share/constain";
+
 export class BankingTransactionModel {
     date: string;
     content: string;
     amount: string;
-    type: string;
+    type: TransactionTypeEnum;
 }
--- transaction-service/src/bank-transaction/bank-transaction.service.ts
+++ transaction-service/src/bank-transaction/bank-transaction.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import {RmqContext} from "@nestjs/microservices";
 import {BankingTransactionModel} from "./bank-transaction.model";
+import {getConnection} from "typeorm";
+import {BankTransactionEntity} from "./bank-transaction.entity";
 
 @Injectable()
 export class BankTransactionService {
@@ -8,16 +10,42 @@ export class BankTransactionService {
 
     /**
      * @description import data from rabbitMq
+     *
+     * insert the data of rabbitMq into database
+     *
+     * after the the validate pipe of api-service, the data of rabbit broker was validated and cleaned
+     *
      */
-    async importData(data: BankingTransactionModel, context: RmqContext) {
+    async importData(data: Array<BankingTransactionModel>, context: RmqContext) {
         const channel = context.getChannelRef();
-        const originMessage = context.getMessage();
-        await new Promise(resolve => {
-            setTimeout(() => {
-                console.log(data);
-                resolve(data);
-            }, 5000)
-        });
-        channel.ack(originMessage);
+        if (data === null || data === undefined || data.length === 0) {
+            // TODO: handle error of type
+            channel.ack(context.getMessage());
+        }
+        const queryRunner = getConnection().createQueryRunner();
+        await queryRunner.startTransaction();
+        try {
+            const bankTransactionEntities: BankTransactionEntity[] = [];
+            data.forEach(item => {
+                const bankTransactionEntity = new BankTransactionEntity();
+                bankTransactionEntity.type = item.type;
+                bankTransactionEntity.content = item.content;
+                bankTransactionEntity.amount = item.amount;
+                bankTransactionEntity.date = item.date;
+                bankTransactionEntities.push(bankTransactionEntity);
+            });
+            await queryRunner.manager.createQueryBuilder()
+                .insert()
+                .into(BankTransactionEntity)
+                .values(bankTransactionEntities)
+                .execute();
+            await queryRunner.commitTransaction();
+        } catch {
+            // TODO: handle error
+            await queryRunner.rollbackTransaction();
+        } finally {
+            await queryRunner.release();
+            channel.ack(context.getMessage());
+        }
     }
 }
